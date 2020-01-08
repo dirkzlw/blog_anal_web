b652787c502de9fd354ae914b9a8941bd370b1d8
Ranger <dirkzh@163.com>
2020-01-08 12:04:15 +0800

    初始提交-将逻辑回归训练的性别权重导入Redis

---
+++ src/main/java/com/blog/anal/BlogAnalApplication.java
@@ -0,0 +1,13 @@
+package com.blog.anal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAnalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAnalApplication.class, args);
+    }
+
+}
